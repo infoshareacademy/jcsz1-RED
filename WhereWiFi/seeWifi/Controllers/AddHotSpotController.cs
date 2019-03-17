76238616029bb30f6091af18d51e1b320e30b626
PatrycjaKoter <patrycjakoter@gmail.com>
2019-03-17 20:58:00 +0100

    add AddHotSpot view

---
+++ WhereWiFi/seeWifi/Controllers/AddHotSpotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace seeWifi.Controllers
+{
+    public class AddHotSpotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
