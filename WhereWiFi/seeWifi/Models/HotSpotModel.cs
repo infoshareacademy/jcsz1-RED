5d2613e61d4141171edf0ed8e9b126e35e530549
Michal Wilanowski <mich.wilanowski@gmail.com>
2019-03-24 11:10:43 +0100

    fixed validation, form accept only number values in latitude and longitude model properties

--- WhereWiFi/seeWifi/Models/HotSpotModel.cs
+++ WhereWiFi/seeWifi/Models/HotSpotModel.cs
@@ -13,9 +13,9 @@ namespace seeWifi.Models
         public string Id { get; set; }
         [Required(ErrorMessage = "Podaj nazwę ulicy")]
         public string LocationName { get; set; }
-        [Required(ErrorMessage = "Podaj szerokość geograficzną")]
+        [Required(ErrorMessage = "Podaj szerokość geograficzną w formacie 54.XXXXX")]
         public string LatitudeX { get; set; }
-        [Required(ErrorMessage = "Podaj długość geograficzną")]
+        [Required(ErrorMessage = "Podaj długość geograficzną w formacie 18.XXXXX")]
         public string LongitudeY { get; set; }
     }
 }
