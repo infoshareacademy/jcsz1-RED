6ca5d3258cf603b90c9da1b6cd58fa509954343b
PatrycjaKoter <patrycjakoter@gmail.com>
2019-03-31 12:23:22 +0200

    menu: changing view fix

---
+++ WhereWiFi/seeWifi/Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace seeWifi.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult SuspiciousHotSpots()
+        {
+            return View();
+        }
+    }
+}
