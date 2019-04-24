533fe04365fc05cd1ce1295a1ffa3ea66df1693d
Michal Wilanowski <mich.wilanowski@gmail.com>
2019-04-24 20:09:47 +0200

    Fixed validation in details view

--- WhereWiFi/WiFi.Library/Models/HotSpotModel.cs
+++ WhereWiFi/WiFi.Library/Models/HotSpotModel.cs
@@ -8,6 +8,8 @@ namespace WiFi.Library.Models
         public int Number { get; set; }
         public string fakeID { get; set; }
         [Required(ErrorMessage = "Podaj nazwę ulicy")]
+        [MinLength(5, ErrorMessage = "Nazwa ulicy powinna być dłuższa niż 5 znaków")]
+        [MaxLength(20, ErrorMessage = "Nazwa ulicy powinna być krótsza niż 20 znaków")]
         public string LocationName { get; set; }
         [Required]
         [Range(54,55, ErrorMessage = "Podaj szerokość geograficzną w przedziale 54-55")]
