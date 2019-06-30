189c7727c26dd231bf9f6196eea9e04586e512ed
PatrycjaKoter <patrycjakoter@gmail.com>
2019-06-30 13:50:44 +0200

    add role to controller

---
+++ WhereWiFi/WiFi.Library/Models/IdentityRoleName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WiFi.Library.Services;
+
+namespace WiFi.Library.Models
+{
+    public static class IdentityRoleName
+    {
+        public const string Admin ="Admin";
+        }
+}
